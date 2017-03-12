782514ff5e3bffec4329eb4e6a03d1289d9a3791
Omar Ezz Ed-Dien <om4rezz@linux.ninja.b0x>
2017-03-12 16:01:07 +0200

    finish Home Activity implementation >> DONE

--- app/src/main/java/com/droid/om4rezz/droidmvp/Home/HomeActivity.java
+++ app/src/main/java/com/droid/om4rezz/droidmvp/Home/HomeActivity.java
@@ -4,8 +4,10 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import android.widget.ProgressBar;
+import android.widget.Toast;
 
 import com.droid.om4rezz.droidmvp.R;
 
@@ -27,33 +29,53 @@ public class HomeActivity extends AppCompatActivity implements HomeView, Adapter
 
     private void initViews() {
         listView = (ListView) findViewById(R.id.mylist);
-        progressBar = (ProgressBar) findViewById(R.id.progressBar);
+        progressBar = (ProgressBar) findViewById(R.id.progress);
 
         presenter = new HomePresenterImpl(this, new FindItemsInteractorImpl());
+
+        initEventDriven();
+    }
+
+    private void initEventDriven() {
+        listView.setOnItemClickListener(this);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        presenter.onResume();
+    }
+
+    @Override
+    protected void onDestroy() {
+        presenter.onDestroy();
+        super.onDestroy();
     }
 
     @Override
     public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-
+        presenter.onItemClicked(position);
     }
 
     @Override
     public void showProgress() {
-
+        progressBar.setVisibility(View.VISIBLE);
+        listView.setVisibility(View.INVISIBLE);
     }
 
     @Override
     public void hideProgress() {
-
+        progressBar.setVisibility(View.INVISIBLE);
+        listView.setVisibility(View.VISIBLE);
     }
 
     @Override
     public void setItems(List<String> items) {
-
+        listView.setAdapter(new ArrayAdapter<>(HomeActivity.this, android.R.layout.simple_list_item_1, items));
     }
 
     @Override
     public void showMessage(String message) {
-
+        Toast.makeText(HomeActivity.this, message, Toast.LENGTH_LONG).show();
     }
 }
--- app/src/main/java/com/droid/om4rezz/droidmvp/Home/HomePresenterImpl.java
+++ app/src/main/java/com/droid/om4rezz/droidmvp/Home/HomePresenterImpl.java
@@ -1,10 +1,12 @@
 package com.droid.om4rezz.droidmvp.Home;
 
+import java.util.List;
+
 /**
  * Created by om4rezz on 3/12/17.
  */
 
-class HomePresenterImpl implements HomePresenter {
+class HomePresenterImpl implements HomePresenter, FindItemsInteractor.OnFinishedListener {
 
     private HomeView homeView;
     private FindItemsInteractor findItemsInteractor;
@@ -16,16 +18,34 @@ class HomePresenterImpl implements HomePresenter {
 
     @Override
     public void onResume() {
+        if (homeView != null) {
+            homeView.showProgress();
+        }
 
+        findItemsInteractor.findItems(this);
     }
 
     @Override
     public void onItemClicked(int position) {
-
+        if (homeView != null) {
+            homeView.showMessage(String.format("Position %d clicked.!", position + 1));
+        }
     }
 
     @Override
     public void onDestroy() {
+        homeView = null;
+    }
 
+    @Override
+    public void onFinished(List<String> items) {
+        if(homeView!=null){
+            homeView.setItems(items);
+            homeView.hideProgress();
+        }
+    }
+
+    public HomeView getHomeView(){
+        return homeView;
     }
 }
